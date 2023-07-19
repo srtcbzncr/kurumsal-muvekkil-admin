b50a1da70b0e0989d3ed99ae0763ecef459025a9
Sertaç Bazancir <bzncrsrtc@gmail.com>
2023-07-19 12:20:19 +0300

    Court create design refactored

--- src/components/createCourt/CreateCourt.jsx
+++ src/components/createCourt/CreateCourt.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './style.css';
-import { FormControl, Stack, Typography, MenuItem, TextField, Button, Select, InputLabel, Box, Breadcrumbs, Link, IconButton, Tooltip } from '@mui/material';
+import { FormControl, Stack, Typography, MenuItem, TextField, Button, Select, InputLabel, Box, Breadcrumbs, Link, Tooltip } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
 import { getActiveCourts, createCourt } from '../../services/CourtService';
 import getAuthHeader from '../../helpers/getAuthHeader';
 import { useTranslation } from 'react-i18next';
@@ -8,6 +9,7 @@ import { Outlet, useNavigate } from 'react-router';
 import { useCookies } from 'react-cookie';
 import ArrowBackSharpIcon from '@mui/icons-material/ArrowBackSharp';
 import createCourtPNG from '../../illustrations/createCourt.png';
+import donePNG from '../../illustrations/done.png';
 import AuthCheck from '../authCheck/AuthCheck';
 import Layout from '../layout/Layout';
 
@@ -20,7 +22,10 @@ const CreateCourt = () => {
     const [activeCourts, setActiveCourts] = useState([]);
     const [selectedParentCourtId, setSelectedParentCourtId] = useState("");
     const [newCourtName, setNewCourtName] = useState(null);
-    const [created, setCreated] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [isDone, setIsDone] = useState(false);
 
     function fetchActiveCourts() {
         getActiveCourts(getAuthHeader(cookie.username, cookie.password), i18n.language).then((response) => {
@@ -34,7 +39,8 @@ const CreateCourt = () => {
                 navigate("/login");
             }
         }).catch((error) => {
-            console.log(error);
+            setIsError(true);
+            setErrorMessage(t("error.undefined"));
         })
     };
 
@@ -51,21 +57,30 @@ const CreateCourt = () => {
     };
 
     function handleSaveClick() {
+        setIsError(false);
+        setErrorMessage("");
+        setIsLoading(true);
         createCourt({ name: newCourtName, parentId: selectedParentCourtId }, getAuthHeader(cookie.username, cookie.password), i18n.language).then((response) => {
             if (response.data.status === 201) {
-                setCreated(true);
+                setIsDone(true);
             }
             else if (response.data.status === 400) {
-                console.log("error");
+                setIsError(true);
+                setErrorMessage(response.data.error.message);
             }
             else if (response.data.status === 401) {
-                console.log("error");
+                setIsError(true);
+                setErrorMessage(response.data.error.message);
             }
             else if (response.data.status === 403) {
-                console.log("error");
+                setIsError(true);
+                setErrorMessage(response.data.error.message);
             }
         }).catch((error) => {
-            console.log(error);
+            setIsError(true);
+            setErrorMessage(t("error.undefined"));
+        }).finally(() => {
+            setIsLoading(false);
         })
     };
 
@@ -73,17 +88,27 @@ const CreateCourt = () => {
         navigate("/courts");
     }
 
+    function handleBackCourtManagementOnClick() {
+        navigate("/courts");
+    }
+
+    function handleNewCourtOnClick() {
+        setIsDone(false);
+        setIsError(false);
+        setErrorMessage("");
+    }
+
     return (
         <AuthCheck>
             <Layout>
-                <Stack direction="column" sx={{ width: 1, height: 1, justifyContent: "flex-start", alignItems: "center" }}>
+                <Stack direction="column" sx={{ width: 1, height: 1, justifyContent: {xs: "center", sm: "flex-start"}, alignItems: {xs: "center", sm: "center"} }}>
                     <Stack id="title" direction={{ xs: "column", sm: "row" }} spacing={2} sx={{ width: 0.8, justifyContent: "space-between", marginTop: "100px" }}>
                         <Typography variant="h4">
                             {t("court.create")}
                         </Typography>
                         <Tooltip title={t("back")}>
                             <Button variant="outlined" color="text" onClick={handleBackClick}>
-                                <ArrowBackSharpIcon color="text"/>
+                                <ArrowBackSharpIcon color="text" />
                             </Button>
                         </Tooltip>
                     </Stack>
@@ -98,34 +123,59 @@ const CreateCourt = () => {
                             <Typography color="text.primary">{t("court.create")}</Typography>
                         </Breadcrumbs>
                     </Box>
-                    <Stack id="main" direction="row" sx={{ width: 0.8, backgroundColor: "secondary.main", marginTop: "25px" }}>
-                        <Box display="flex" sx={{ width: 0.5, justifyContent: "center" }}>
-                            <img src={createCourtPNG} width="90%"></img>
-                        </Box>
-                        <Stack direction="column" sx={{ width: 0.5, justifyContent: "center", paddingRight: "50px" }}>
-                            <Typography variant="h5">
-                                {t("court.infos")}
-                            </Typography>
-                            <FormControl sx={{ marginTop: "40px"}}>
-                                <InputLabel id="parent-court-select-label">{t("court.parent")}</InputLabel>
-                                <Select
-                                    labelId="parent-court-select-label"
-                                    id="parent-court-select"
-                                    value={selectedParentCourtId}
-                                    label={t("court.parent")}
-                                    onChange={handleSelectedParentCourtChange}
-                                >
-                                    {
-                                        activeCourts.map((court) => <MenuItem key={court.id} value={court.id}>{court.name}</MenuItem>)
-                                    }
-                                </Select>
-                                <TextField required id="court-name" label={t("name")} variant="outlined" sx={{ marginTop: "20px"}} value={newCourtName} onChange={handleNewCourtNameChange} /> 
-                                <Stack direction="row" sx={{ width: 1, justifyContent: "flex-end", marginTop: "30px" }}>
-                                    <Button variant="contained" size="large" sx={{ width: 0.4}} onClick={handleSaveClick}>{t("save")}</Button>
+                    {
+                        isDone === false
+                            ?
+                            <Stack id="main" direction={{ xs: "column", sm: "row"}} sx={{ width: 0.8, justifContent: "center", alignItems: "center", backgroundColor: "secondary.main", marginTop: "25px", paddingRight: {xs: "0px", sm: "20px"}, paddingTop: "20px", paddingBottom: "20px" }}>
+                                <Box display="flex" sx={{ width: 0.5, justifyContent: "center" }}>
+                                    <img src={createCourtPNG} width="90%"></img>
+                                </Box>
+                                <Stack direction="column" sx={{ width: 0.5, justifyContent: "center", alignItems: "center", paddingTop: {xs: "20px", sm: "0px"}}}>
+                                    <Typography variant="h5" sx={{ width: 0.7 }}>
+                                        {t("court.infos")}
+                                    </Typography>
+                                    <FormControl sx={{ marginTop: "40px", width: 0.7 }}>
+                                        <InputLabel id="parent-court-select-label">{t("court.parent")}</InputLabel>
+                                        <Select
+                                            labelId="parent-court-select-label"
+                                            id="parent-court-select"
+                                            value={selectedParentCourtId}
+                                            label={t("court.parent")}
+                                            onChange={handleSelectedParentCourtChange}
+                                        >
+                                            {
+                                                activeCourts.map((court) => <MenuItem key={court.id} value={court.id}>{court.name}</MenuItem>)
+                                            }
+                                        </Select>
+                                        <TextField required id="court-name" label={t("name")} variant="outlined" sx={{ marginTop: "20px" }} value={newCourtName} onChange={handleNewCourtNameChange} />
+                                        <Stack direction="row" sx={{ width: 1, justifyContent: "flex-end", marginTop: "30px" }}>
+                                            {
+                                                isLoading === true
+                                                    ? <LoadingButton loading variant="contained" size="large" sx={{ width: 0.4 }}>{t("save")}</LoadingButton>
+                                                    : <Button variant="contained" size="large" sx={{ width: 0.4 }} onClick={handleSaveClick}>{t("save")}</Button>
+                                            }
+                                        </Stack>
+                                    </FormControl>
                                 </Stack>
-                            </FormControl>
-                        </Stack>
-                    </Stack>
+                            </Stack>
+                            :
+                            <Stack id="main" direction={{ xs: "column", sm: "row"}} sx={{ width: 0.8, justifContent: "center", alignItems: "center", backgroundColor: "secondary.main", marginTop: "25px", paddingRight: {xs: "0px", sm: "20px"}, paddingTop: "20px", paddingBottom: "20px" }}>
+                                <Box display="flex" sx={{ width: 0.5, justifyContent: "center" }}>
+                                    <img src={donePNG} width="90%"></img>
+                                </Box>
+                                <Stack direction="column" sx={{ width: 0.5, justifyContent: "center", alignItems: "center", paddingTop: {xs: "20px", sm: "0px"}}}>
+                                    <Stack direction="column" spacing={8} sx={{ width: 1, alignItems: "center"}}>
+                                        <Typography variant='h5'>
+                                            {t("court.create.successful")}
+                                        </Typography>
+                                        <Stack direction="row" spacing={2} sx={{ width: 1, justifyContent:"center" }}>
+                                            <Button variant="contained" size="large" color="text" sx={{ color: "secondary.main" }} onClick={handleNewCourtOnClick}>{t("court.create")}</Button>
+                                            <Button variant="outlined" size="large" color="text" onClick={handleBackCourtManagementOnClick}>{t("back.court.management")}</Button>
+                                        </Stack>
+                                    </Stack>
+                                </Stack>
+                            </Stack>
+                    }
                 </Stack>
             </Layout>
             <Outlet />
